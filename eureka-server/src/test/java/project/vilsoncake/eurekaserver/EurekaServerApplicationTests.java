6b9d83dff1e8726ea6d444ec2c6ec0957b4f6c61
ViLsonCake <timofeydmitrenko13@gmail.com>
2024-02-05 22:04:31 +0100

    Add Eureka server module

---
+++ eureka-server/src/test/java/project/vilsoncake/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.vilsoncake.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
