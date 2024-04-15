e54ca0c4bfd987805aca2fc938fcbc32939eab03
ViLsonCake <timofeydmitrenko13@gmail.com>
2024-04-15 22:35:41 +0200

    add default categories

---
+++ todo-category-service/src/main/java/project/vilsoncake/todocategoryservice/constant/DefaultCategory.java
@@ -0,0 +1,12 @@
+package project.vilsoncake.todocategoryservice.constant;
+
+import java.util.List;
+
+public class DefaultCategory {
+    public static final List<String> defaultCategories = List.of(
+            "Home",
+            "Work",
+            "Education",
+            "Important"
+    );
+}
