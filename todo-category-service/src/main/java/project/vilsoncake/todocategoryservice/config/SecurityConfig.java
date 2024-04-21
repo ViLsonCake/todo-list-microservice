02eb6e9a7c721cbecda97ff3fb1fe3887237c216
ViLsonCake <timofeydmitrenko13@gmail.com>
2024-04-21 20:17:41 +0200

    disable csrf on security config

--- todo-category-service/src/main/java/project/vilsoncake/todocategoryservice/config/SecurityConfig.java
+++ todo-category-service/src/main/java/project/vilsoncake/todocategoryservice/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,6 +23,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests(auth -> auth
                 .anyRequest()
                 .authenticated()
