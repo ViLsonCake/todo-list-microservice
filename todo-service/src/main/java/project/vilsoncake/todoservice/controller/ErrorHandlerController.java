0ec7f00f0f3019751b9d361fa23fafc8fac89935
ViLsonCake <timofeydmitrenko13@gmail.com>
2024-04-28 22:14:05 +0200

    add new exception handler

--- todo-service/src/main/java/project/vilsoncake/todoservice/controller/ErrorHandlerController.java
+++ todo-service/src/main/java/project/vilsoncake/todoservice/controller/ErrorHandlerController.java
@@ -6,6 +6,8 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import project.vilsoncake.todoservice.exception.IncorrectTodoFilterException;
+import project.vilsoncake.todoservice.exception.TodoNotFoundException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,5 +24,17 @@ public class ErrorHandlerController {
             errors.put(fieldName, errorMessage);
         });
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> todoNotFoundException(TodoNotFoundException exception) {
+        Map<String, String> body = Map.of("message", exception.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> incorrectTodoFilterException(IncorrectTodoFilterException exception) {
+        Map<String, String> body = Map.of("message", exception.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
 }
