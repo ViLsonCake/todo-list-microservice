a5b036b73e60982defc6eb7b36814aa2355e12c4
ViLsonCake <timofeydmitrenko13@gmail.com>
2024-05-06 00:54:35 +0200

    add change username tests

--- authorization-server/src/test/java/project/vilsoncake/authorizationserver/controller/UserControllerTest.java
+++ authorization-server/src/test/java/project/vilsoncake/authorizationserver/controller/UserControllerTest.java
@@ -1,28 +1,30 @@
 package project.vilsoncake.authorizationserver.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dasniko.testcontainers.keycloak.KeycloakContainer;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.apache.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
+import project.vilsoncake.authorizationserver.dto.TokenDto;
 import project.vilsoncake.authorizationserver.entity.UserEntity;
 import project.vilsoncake.authorizationserver.repository.UserRepository;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.StreamSupport;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @ExtendWith(SpringExtension.class)
@@ -35,6 +37,9 @@ class UserControllerTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     static final KeycloakContainer keycloakContainer = new KeycloakContainer("quay.io/keycloak/keycloak:22.0.5")
             .withRealmImportFile("keycloak/realm-export.json");
@@ -50,6 +55,20 @@ class UserControllerTest {
     static void stopContainers() {
         keycloakContainer.stop();
         postgresqlContainer.stop();
+    }
+
+    @BeforeEach
+    void initUser() {
+        UserEntity user = new UserEntity();
+        user.setId(UUID.randomUUID().toString());
+        user.setUsername("testuser");
+        user.setEmail("testuser@gmail.com");
+        userRepository.save(user);
+    }
+
+    @AfterEach
+    void deleteUser() {
+        userRepository.deleteAll();
     }
 
     @DynamicPropertySource
@@ -133,5 +152,89 @@ class UserControllerTest {
         assertEquals(response.getResponse().getStatus(), HttpStatus.SC_CONFLICT);
         assertTrue(response.getResponse().getContentAsString().contains("\"message\":"));
         assertEquals(usersBeforeRequest.size(), usersAfterRequest.size());
+    }
+
+    @Test
+    @DisplayName("Change user username test with valid new username")
+    void changeUserUsername_validUserData() throws Exception {
+        String username = "currentUsername";
+        String newUsername = "newUsername";
+        String email = "changeuser@gmail.com";
+        String password = "testpass";
+        String createUserRequest = String.format("{\"username\":\"%s\",\"email\":\"%s\",\"password\":\"%s\"}", username, email, password);
+        String tokenRequest = "{\"username\":\"%s\",\"password\":\"%s\"}";
+
+        var response = mockMvc.perform(
+                post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(createUserRequest)
+        ).andReturn();
+
+        var tokenResponse = mockMvc.perform(
+                post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(String.format(tokenRequest, username, password))
+        ).andReturn();
+
+        String accessToken = objectMapper.readValue(tokenResponse.getResponse().getContentAsString(), TokenDto.class).getAccessToken();
+        String jsonChangeUsernameRequest = String.format("{\"newUsername\":\"%s\"}", newUsername);
+
+        var changeUserResponse = mockMvc.perform(
+                patch("/users")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonChangeUsernameRequest)
+        ).andReturn();
+
+        var changedUsernameTokenResponse = mockMvc.perform(
+                post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(String.format(tokenRequest, newUsername, password))
+        ).andReturn();
+
+        // When
+        assertEquals(changeUserResponse.getResponse().getStatus(), HttpStatus.SC_OK);
+        assertEquals(changedUsernameTokenResponse.getResponse().getStatus(), HttpStatus.SC_OK);
+        assertTrue(response.getResponse().getContentAsString().contains("\"message\":"));
+        assertNull(userRepository.findByUsernameIgnoreCase(username));
+        assertNotNull(userRepository.findByUsernameIgnoreCase(newUsername));
+    }
+
+    @Test
+    @DisplayName("Change user username test with already used new username")
+    void changeUserUsername_invalidUserData() throws Exception {
+        String username = "username";
+        String newUsedUsername = "testuser";
+        String email = "changeuserinvalid@gmail.com";
+        String password = "testpass";
+        String createUserRequest = String.format("{\"username\":\"%s\",\"email\":\"%s\",\"password\":\"%s\"}", username, email, password);
+        String tokenRequest = "{\"username\":\"%s\",\"password\":\"%s\"}";
+
+        var response = mockMvc.perform(
+                post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(createUserRequest)
+        ).andReturn();
+
+        var tokenResponse = mockMvc.perform(
+                post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(String.format(tokenRequest, username, password))
+        ).andReturn();
+
+        String accessToken = objectMapper.readValue(tokenResponse.getResponse().getContentAsString(), TokenDto.class).getAccessToken();
+        String jsonChangeUsernameRequest = String.format("{\"newUsername\":\"%s\"}", newUsedUsername);
+
+        var changeUserResponse = mockMvc.perform(
+                patch("/users")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonChangeUsernameRequest)
+        ).andReturn();
+
+        // When
+        assertEquals(HttpStatus.SC_CONFLICT, changeUserResponse.getResponse().getStatus());
+        assertTrue(response.getResponse().getContentAsString().contains("\"message\":"));
+        assertNotNull(userRepository.findByUsernameIgnoreCase(username));
     }
 }
